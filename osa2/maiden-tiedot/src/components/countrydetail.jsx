84f326e09421d43755b18f1d0852984ca8173e5b
JonesTPG <joonas.ryynanen1@gmail.com>
2019-08-13 18:26:13 +0300

    Maiden tiedot tehty.

--- osa2/maiden-tiedot/src/components/countrydetail.jsx
+++ osa2/maiden-tiedot/src/components/countrydetail.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CountryDetail = ({ countryData }) => {
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://api.apixu.com/v1/current.json?key=f83e894e97624aea833145251191308&q=" +
+          countryData.capital
+      )
+      .then(response => {
+        console.log(response.data);
+        setWeatherData(response.data);
+      });
+  }, []);
+
   return (
     <>
-      <h3>{countryData.name}</h3>
-      <br />
-      <p>capital {countryData.capital}</p>
-      <p>population {countryData.population}</p>
-      <h4>languages</h4>
-      <ul>
-        {countryData.languages.map(language => (
-          <li key={language.name}>{language.name}</li>
-        ))}
-      </ul>
-      <img src={countryData.flag} height="100" width="200" alt="flag" />
+      {weatherData.current == undefined ? (
+        <p>not rendered yet</p>
+      ) : (
+        <>
+          <h3>{countryData.name}</h3>
+          <br />
+          <p>capital {countryData.capital}</p>
+          <p>population {countryData.population}</p>
+          <h4>languages</h4>
+          <ul>
+            {countryData.languages.map(language => (
+              <li key={language.name}>{language.name}</li>
+            ))}
+          </ul>
+          <img src={countryData.flag} height="100" width="200" alt="flag" />
+          <h4>Weather in {countryData.capital}</h4>
+          <h5>temperature: {weatherData.current.temp_c} Celsius</h5>
+          <img src={"https:" + weatherData.current.condition.icon} alt="icon" />
+          <h5>
+            wind: {weatherData.current.wind_kph} kph direction{" "}
+            {weatherData.current.wind_dir}
+          </h5>
+        </>
+      )}
     </>
   );
 };

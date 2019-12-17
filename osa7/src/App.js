7c75447630ff105915344867b3a430d29be7594d
JonesTPG <joonas.ryynanen1@gmail.com>
2019-12-17 09:27:19 +0200

    7.1 tehty.

---
+++ osa7/src/App.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+
+import About from "./components/about";
+import CreateAnecdote from "./components/createnew";
+import Footer from "./components/footer";
+import AnecdoteList from "./components/anectodelist";
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  };
+  return (
+    <div>
+      <Link to="/">anecdotes</Link> {"   "}
+      <Link to="/create">create new</Link>
+      {"   "}
+      <Link to="/about">about</Link>
+    </div>
+  );
+};
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: "If it hurts, do it more often",
+      author: "Jez Humble",
+      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
+      votes: 0,
+      id: "1"
+    },
+    {
+      content: "Premature optimization is the root of all evil",
+      author: "Donald Knuth",
+      info: "http://wiki.c2.com/?PrematureOptimization",
+      votes: 0,
+      id: "2"
+    }
+  ]);
+
+  const [notification, setNotification] = useState("");
+
+  const addNew = anecdote => {
+    anecdote.id = (Math.random() * 10000).toFixed(0);
+    setAnecdotes(anecdotes.concat(anecdote));
+  };
+
+  const anecdoteById = id => anecdotes.find(a => a.id === id);
+
+  const vote = id => {
+    const anecdote = anecdoteById(id);
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    };
+
+    setAnecdotes(anecdotes.map(a => (a.id === id ? voted : a)));
+  };
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+
+      <Router>
+        <Menu />
+        <Route
+          exact
+          path="/"
+          render={() => <AnecdoteList anecdotes={anecdotes} />}
+        />
+        <Route
+          exact
+          path="/create"
+          render={() => <CreateAnecdote addNew={addNew} />}
+        />
+        <Route exact path="/about" render={() => <About />} />
+        {/* <Route
+          exact
+          path="/anecdotes/:id"
+          render={({ match }) => (
+            <Anecdote anecdote={anecdoteById(match.params.id)} />
+          )}
+        /> */}
+      </Router>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;

84f674f88793c7870f32d8731b78f8c0af5d3b32
JonesTPG <joonas.ryynanen1@gmail.com>
2019-12-21 18:56:40 +0200

    6a: voting works, app is structured into components.

---
+++ osa6-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AnecdoteForm = ({ store }) => {
+  return (
+    <>
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </>
+  );
+};
+
+export default AnecdoteForm;

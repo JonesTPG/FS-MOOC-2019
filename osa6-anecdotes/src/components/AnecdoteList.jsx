acd8ef357884953ee05cae173cdcc303df64a2c5
JonesTPG <joonas.ryynanen1@gmail.com>
2019-12-21 19:11:48 +0200

    6.5: sorting functionality.

--- osa6-anecdotes/src/components/AnecdoteList.jsx
+++ osa6-anecdotes/src/components/AnecdoteList.jsx
@@ -7,17 +7,19 @@ const AnecdoteList = ({ store }) => {
 
   return (
     <>
-      {anecdotes.map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes} votes
-            <button onClick={() => store.dispatch(voteAnecdote(anecdote.id))}>
-              vote
-            </button>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes} votes
+              <button onClick={() => store.dispatch(voteAnecdote(anecdote.id))}>
+                vote
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 };

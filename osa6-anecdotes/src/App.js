1120573b8a7e2ef4362277ea9779dff654d18349
JonesTPG <joonas.ryynanen1@gmail.com>
2019-12-26 09:51:08 +0200

    6.16 tehty.

--- osa6-anecdotes/src/App.js
+++ osa6-anecdotes/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
 import Filter from "./components/Filter";
 
-const App = () => {
+import anecdoteService from "./services/anecdotes";
+
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
+
+const App = props => {
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then(anecdotes => props.initializeAnecdotes(anecdotes));
+  }, [props]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -17,4 +28,12 @@ const App = () => {
   );
 };
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    initializeAnecdotes: value => {
+      dispatch(initializeAnecdotes(value));
+    }
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);

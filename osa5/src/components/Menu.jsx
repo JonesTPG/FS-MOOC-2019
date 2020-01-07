77814815e7aaf22004c749722d71ea4f8b9bbdf3
JonesTPG <joonas.ryynanen1@gmail.com>
2020-01-07 15:33:00 +0200

    7.10 tehty.

--- osa5/src/components/Menu.jsx
+++ osa5/src/components/Menu.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <>
-      <p> valikko </p>
+      <p>
+        {" "}
+        <Link to="/users">Users</Link> <Link to="/blogs">Blogs</Link>{" "}
+      </p>
     </>
   );
 };
